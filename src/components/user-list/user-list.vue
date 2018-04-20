1ee10e57fc8b0276cf76b547f52433f2b2ef5275
LemonTeaQueen <1125542883@qq.com>
2018-04-20 11:20:34 +0800

    用户列表设置与axios配置请求头携带Token

--- src/components/user-list/user-list.vue
+++ src/components/user-list/user-list.vue
@@ -1,13 +1,101 @@
 <template>
-<div><p>用户列表组件</p></div>
+  <div class="user-list-wrap">
+    <el-row>
+      <el-col :span="24">
+        <el-breadcrumb separator-class="el-icon-arrow-right" class="user-list-breadcrumb">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>活动管理</el-breadcrumb-item>
+          <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+          <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+        </el-breadcrumb>
+      </el-col>
+    </el-row>
+    <el-row class="user-list-search">
+      <el-col :span="8">
+        <el-input placeholder="请输入内容" v-model="searchText" class="input-with-select">
+          <el-button slot="append" icon="el-icon-search"></el-button>
+        </el-input>
+      </el-col>
+      <el-col :span="2">
+        <el-button type="success" plain>添加用户</el-button>
+      </el-col>
+    </el-row>
+    <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        prop="role_name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="email"
+        label="邮箱"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="mobile"
+        label="电话">
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage4"
+      :page-sizes="[100, 200, 300, 400]"
+      :page-size="100"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="400"
+      class="user-list-pagination">
+    </el-pagination>
+  </div>
 </template>
 <script>
+import axios from 'axios'
 export default {
+  async created () {
+    const {token} = JSON.parse(window.localStorage.getItem('admin-token'))
+    // console.log(token)
+    const res = await axios.get('http://localhost:8888/api/private/v1/users', {
+      headers: {
+        Authorization: token
+      },
+      params: {
+        pagenum: 1,
+        pagesize: 5
+      }
+    })
+    this.tableData = res.data.data.users
+  },
+  methods: {
+    handleSizeChange (val) {
+      console.log(`每页 ${val} 条`)
+    },
+    handleCurrentChange (val) {
+      console.log(`当前页: ${val}`)
+    }
+  },
   data () {
-    return {}
+    return {
+      searchText: '',
+      tableData: [],
+      currentPage1: 5,
+      currentPage2: 5,
+      currentPage3: 5,
+      currentPage4: 4
+    }
   }
 }
 </script>
 
 <style>
+.user-list-breadcrumb{
+  line-height: 3;
+}
+.user-list-search{
+  padding: 10px 0;
+}
+.user-list-pagination{
+  padding: 10px  0;
+}
 </style>
